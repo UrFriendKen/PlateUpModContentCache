4343aca670ea1a028b5f078b432d8f33da5b70ff
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2024-03-13 10:04:33 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿using KitchenMods;
+using System.IO;
+using UnityEngine;
+
+// Namespace should have "Kitchen" in the beginning
+namespace ModContentCache
+{
+    public class Main : IModInitializer
+    {
+        public const string MOD_GUID = $"IcedMilo.PlateUp.{MOD_NAME}";
+        public const string MOD_NAME = "Mod Content Cache";
+        public const string MOD_VERSION = "0.1.0";
+
+        internal const string FOLDER_NAME = "ModContentCache";
+
+        internal static string FolderPath => Path.Combine(Application.persistentDataPath, FOLDER_NAME);
+
+        public void PostActivate(KitchenMods.Mod mod)
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        public void PreInject()
+        {
+            ModInfoRegistry.Initialise();
+        }
+
+        public void PostInject()
+        {
+        }
+
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
---
+++ ModInfoRegistry.cs
@@ -0,0 +1,239 @@
+﻿using KitchenData;
+using KitchenMods;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Unity.Entities;
+
+namespace ModContentCache
+{
+    public static class ModInfoRegistry
+    {
+        private const string PRELOAD_FILE_NAME = "ModInfoRegistry_Preload.json";
+        private const string SAVE_FILE_NAME = "ModInfoRegistry.json";
+
+        private static Dictionary<ulong, ModInfo> _modInfos = new Dictionary<ulong, ModInfo>();
+        private static Dictionary<ulong, ModInfo> ModInfos
+        {
+            get
+            {
+                if (_modInfos == null)
+                {
+                    Initialise();
+                }
+                return _modInfos;
+            }
+        }
+        public struct ModMetadata
+        {
+            public string Name;
+
+            public ulong ID;
+        }
+
+        private class ModInfo
+        {
+            public string Name;
+            public ulong ID;
+
+            [JsonProperty]
+            private Dictionary<ulong, string> UnityTypeHashes;
+
+            [JsonIgnore]
+            public int UnityTypeHashCount => UnityTypeHashes?.Count ?? 0;
+
+            [JsonIgnore]
+            public bool HasContent => UnityTypeHashes.Any();
+
+            public ModInfo()
+            {
+                Name = "Unknown";
+                ID = 0;
+                UnityTypeHashes = new Dictionary<ulong, string>();
+            }
+
+            public ModInfo(string name, ulong id)
+            {
+                Name = name;
+                ID = id;
+                UnityTypeHashes = new Dictionary<ulong, string>();
+            }
+
+            public ModInfo(Mod mod)
+            {
+                Name = mod.Name;
+                ID = mod.ID;
+                UnityTypeHashes = new Dictionary<ulong, string>();
+
+                IEnumerable<Type> types = mod.GetPacks<AssemblyModPack>().SelectMany(x => x.Asm.GetTypes());
+                try
+                {
+                    UnityTypeHashes = TypeManager.GetAllTypes()
+                        .Where(x => types.Contains(x.Type))
+                        .ToDictionary(x => x.StableTypeHash, x => x.Type.Name);
+                }
+                catch (Exception ex)
+                {
+                    Main.LogError($"Failed to populate UnityTypeHashes for {Name ?? "Unknown"} ({ID})\n{ex.Message}\n{ex.StackTrace}");
+                }
+            }
+
+            public static ModInfo FromMod(Mod mod)
+            {
+                return new ModInfo(mod);
+            }
+
+            public ModInfo MergeWith(ModInfo other)
+            {
+                foreach (KeyValuePair<ulong, string> kvp in other.UnityTypeHashes)
+                    UnityTypeHashes[kvp.Key] = kvp.Value;
+
+                return this;
+            }
+
+            public bool IsUnityTypeSource(ulong stableTypeHash)
+            {
+                return UnityTypeHashes?.ContainsKey(stableTypeHash) ?? false;
+            }
+
+            public bool IsUnityTypeSource(IEnumerable<ulong> stableTypeHashes)
+            {
+                return UnityTypeHashes?.Select(x => x.Key).Intersect(stableTypeHashes).Any() ?? false;
+            }
+        }
+
+        internal static void Initialise()
+        {
+            _modInfos.Clear();
+
+            if (!Directory.Exists(Main.FolderPath))
+                Directory.CreateDirectory(Main.FolderPath);
+
+            void AddToModInfos(IEnumerable<ModInfo> modInfosToAdd)
+            {
+                foreach (ModInfo modInfo in modInfosToAdd)
+                {
+                    if (!_modInfos.TryGetValue(modInfo.ID, out ModInfo existingModInfo))
+                    {
+                        _modInfos[modInfo.ID] = modInfo;
+                        continue;
+                    }
+                    existingModInfo.MergeWith(modInfo);
+                }
+            }
+
+            bool TryLoadFromFile(string filename, out Dictionary<ulong, ModInfo> modInfos)
+            {
+                string filepath = Path.Combine(Main.FolderPath, filename);
+                if (File.Exists(filepath))
+                {
+                    try
+                    {
+                        modInfos = Deserialise(File.ReadAllText(filepath));
+                        Main.LogInfo($"Load from {filepath}.");
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Main.LogError($"Failed to load {filepath}\n{ex.Message}\n{ex.StackTrace}");
+                    }
+                }
+                else
+                {
+                    Main.LogWarning($"Skipping load from {filepath}. File does not exist.");
+                }
+                modInfos = default;
+                return false;
+            }
+
+            Dictionary<ulong, ModInfo> Deserialise(string data)
+            {
+                return JsonConvert.DeserializeObject<Dictionary<ulong, ModInfo>>(data);
+            }
+
+            bool TryReadFromPreload(out Dictionary<ulong, ModInfo> modInfos)
+            {
+                try
+                {
+                    using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"ModContentCache.Preloads.{PRELOAD_FILE_NAME}");
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        modInfos = Deserialise(reader.ReadToEnd());
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Main.LogError($"{ex.Message}\n{ex.StackTrace}");
+                    modInfos = default;
+                    return false;
+                }
+            }
+
+            AddToModInfos(ModPreload.Mods
+                .Where(mod => mod.ID != 0)
+                .Select(ModInfo.FromMod));
+
+            if (TryReadFromPreload(out var preloadModInfos))
+            {
+                AddToModInfos(preloadModInfos.Values);
+            }
+
+            foreach (string filename in new string[] { SAVE_FILE_NAME })
+            {
+                if (TryLoadFromFile(filename, out var modInfos))
+                    AddToModInfos(modInfos.Values);
+            }
+
+            File.WriteAllText(Path.Combine(Main.FolderPath, SAVE_FILE_NAME), JsonConvert.SerializeObject(_modInfos.Where(kvp => kvp.Value.HasContent).ToDictionary(kvp => kvp.Key, kvp => kvp.Value), Formatting.Indented));
+        }
+
+        public static bool FindUnityTypeHashSource(ulong hash, out ModMetadata modMetadata)
+        {
+            modMetadata = default;
+            var foundModInfos = ModInfos.Select(x => x.Value).Where(modInfo => modInfo.IsUnityTypeSource(hash));
+            if (!foundModInfos.Any())
+                return false;
+
+            ModInfo matchingModInfo = foundModInfos.First();
+            modMetadata = new ModMetadata()
+            {
+                Name = matchingModInfo.Name,
+                ID = matchingModInfo.ID
+            };
+            return true;
+        }
+
+        public static bool FindUnityTypeHashSource(IEnumerable<ulong> hashes, out IEnumerable<ModMetadata> modMetadatas)
+        {
+            modMetadatas = default;
+            if (!hashes.Any())
+            {
+                Main.LogWarning("\tNo hashes");
+                return false;
+            }
+
+            var foundModInfos = ModInfos.Select(x => x.Value).Where(modInfo => modInfo.IsUnityTypeSource(hashes));
+            if (!foundModInfos.Any())
+            {
+                Main.LogWarning("\tNo mod infos found");
+                return false;
+            }
+
+            modMetadatas = foundModInfos.Select(modInfo => new ModMetadata()
+            {
+                Name = modInfo.Name,
+                ID = modInfo.ID
+            });
+            return true;
+        }
+
+        public static string GetModName(ulong modID)
+        {
+            return ModInfos.TryGetValue(modID, out ModInfo modInfo) ? modInfo.Name : $"Unknown ({modID})";
+        }
+    }
+}
